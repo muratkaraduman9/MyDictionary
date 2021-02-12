508fa43c35fb30d35dbb991050c67635041015d9
Murat Karaduman <karadumanmurat9@gmail.com>
2021-02-12 18:50:30 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryProject
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempKeys;
+        TValue[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+        public void ToList()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " -> " + values[i]);
+            }
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MyDictionaryProject;
+using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> customers = new MyDictionary<int, string>();
+            customers.Add(10, "Murat");
+            customers.Add(94, "Talisca");
+            customers.Add(33, "Gomez");
+            customers.ToList();
+            
+        }
+    }
+}
